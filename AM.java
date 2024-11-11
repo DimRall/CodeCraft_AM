5396811ad0529ee2af216b4b75ea603ebd2b6bde
panagiotisanastopoulos <t8220007@aueb.gr>
2024-11-11 14:41:35 +0200

    Eμφανιση ΑΜ Παναγιωτη Αναστοπουλου

--- AM.java
+++ AM.java
@@ -1,5 +1,6 @@
 public class AM {
     public static void main(String[]args){
         System.out.println("Δημήτρης Ράλλης -> 8220229");
+        System.out.println("Παναγιωτης Αναστοπουλος = 8220007");
     }
 } 
