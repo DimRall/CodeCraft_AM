77a3d1e52a1060f0fc254408f2fc8848a4d543b7
Panagiotis Kakaletris <t8220053@aueb.gr>
2024-11-11 17:09:47 +0200

    AM  παναγιώτη κακαλέτρη

--- AM.java
+++ AM.java
@@ -1,10 +1,11 @@
 public class AM {
     public static void main(String[]args){
         System.out.println("Δημήτρης Ράλλης -> 8220229");
-        System.out.println("Παναγιωτης Αναστοπουλος = 8220007");
+        System.out.println("Παναγιώτης Αναστόπουλος = 8220007");
         System.out.println("Βασιλείου Ιοκάστη ->8230013");
         System.out.println("Ντερτινης Αποστολος = 8220108");
         System.out.println("Αχνιώτης Σταύρος = 8230231");
         System.out.println("Σταματοπούλου Χρύσα Δέσποινα = 8230139");
+        System.out.println("Κακαλέτρης Παναγιώτης = 8220053 ");
     }
 }
